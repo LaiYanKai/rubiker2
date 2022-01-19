1983b8aa460c819b68c674a578477624716bc627
LaiYanKai <lai.yankai@gmail.com>
2022-01-19 09:21:54 +0000

    220119

--- run.sh
+++ run.sh
@@ -1,16 +1,15 @@
 #PARAMETERS
 source devel/setup.bash
 echo "BASH SOURCED"
-sudo ip ad add 10.0.0.2/24 dev eth0
+sudo ip addr add 10.0.0.2/24 dev eth0
 sudo ip link set dev eth0 up
 export ROS_MASTER_URI=http://10.0.0.1:11311
 export ROS_HOSTNAME=10.0.0.2
 
-#		MTR	K(BCM)	W(BCM)	Y(BCM)	B(BCM)	KP	KI	KD	ErrThrs	LR Verbose
-export MotorFH="FH	3	2	18	23	2	0.001	2	5	40 0"
-export MotorBH="BH	25	24	27	16	2	0.001	2	5	40 0"
-export MotorFW="FW	7	8	20	21	2	0.001	3	5	40 1"
-export MotorBW="BW	6	5	19	13	2	0.001	3	5	40 1"
-export MotorPP="PP	22	10	11	9	0.2	0.001	0.3	5	40 0"
+# 		MTR	K(BCM)	W(BCM)	Y(BCM)	B(BCM)	KP	KI	KD	ErrThrs	LR Verbose
+export MotorFH="FH	3	2	23	18	2.0	0.01	2.0	5	40 1" #M1 #+Close -Open
+export MotorBH="BH	25	24	16	27	1.0	0.01	2.0	5	40 1" #M2 #+C -O
+export MotorFW="FW	7	8	21	20	2	0.001	3	5	40 1" #M3 #+ACW -CW
+export MotorBW="BW	6	5	13	19	2	0.001	3	5	40 1" #M4 #+ACW -CW
+export MotorPP="PP	10	22	11	9	0.15	0.000	0.0	5	40 1" #M5 #+D -U
 roslaunch rubiker2 run.launch
-
--- test.sh
+++ test.sh
@@ -6,9 +6,9 @@ export ROS_MASTER_URI=http://localhost:11311
 export ROS_HOSTNAME=localhost
 
 # 		MTR	K(BCM)	W(BCM)	Y(BCM)	B(BCM)	KP	KI	KD	ErrThrs	LR Verbose
-#export Motor="FH	3	2	18	23	2.0	0.01	2.0	5	40 1"
-#export Motor="BH	25	24	27	16	1.0	0.01	2.0	5	40 1"
-export Motor="FW	7	8	21	20	2	0	2	5	40 1"
-#export Motor="BW	6	5	19	13	1.5	0.001	2.0	5	40 1"
-#export Motor="PP	22	10	11	9	0.15	0.000	0.0	5	40 1"
+#export Motor="FH	3	2	23	18	2.0	0.01	2.0	5	40 1" #M1 #+Close -Open
+#export Motor="BH	25	24	16	27	1.0	0.01	2.0	5	40 1" #M2 #+C -O
+#export Motor="FW	7	8	21	20	2	0.001	3	5	40 1" #M3 #+ACW -CW
+#export Motor="BW	6	5	13	19	2	0.001	3	5	40 1" #M4 #+ACW -CW
+export Motor="PP	10	22	11	9	0.15	0.000	0.0	5	40 1" #M5 #+D -U
 roslaunch rubiker2 test.launch
